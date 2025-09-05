5785f921a00a5357f4c07e26554fc5c5d01bf4b8
Rico Xavier <rico@Ricos-MacBook-Pro-2.local>
2025-09-05 15:16:55 +0800

    Add Facebook Pixel ID 746886027994806 with ViewContent tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,29 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
+        {/* Facebook Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '746886027994806');
+              fbq('track', 'ViewContent');
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=746886027994806&ev=ViewContent&noscript=1"
+          />
+        </noscript>
       </head>
       <body className="bg-white antialiased">{children}</body>
     </html>
